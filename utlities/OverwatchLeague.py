ba8e1fa284a8c90d9a15de6cb6279a28357593b6
Pidiaries <iainjohnston79@gmail.com>
2019-10-25 08:48:36 +0100

    auto_pi

--- utlities/OverwatchLeague.py
+++ utlities/OverwatchLeague.py
@@ -12,19 +12,37 @@ reddit = praw.Reddit('script', user_agent='script user agent')
 
 if os.path.exists("../Overwatchleague/owl.xml"):
     os.remove('../Overwatchleague/owl.xml')
-for submission in reddit.subreddit('OverwatchLeague').search('site:youtube.com', limit=100, sort='hot', time_filter='all'):
+for submission in reddit.subreddit('OverwatchLeague').search('site:youtube.com OR site:clips.twitch.tv', limit=100, sort='hot', time_filter='all'):
     print(submission.title)
-    with open('../Overwatchleague/owl.xml', 'a') as file:
-        #file.write('<item>\n<title>{}</title>\n<link>{}</link>\n</item>\n  '.format(submission.title, submission.url))
-        file.write('<item>\n<title>')
-        file.write(submission.title)
-        file.write('</title>\n<link>')
-        file.write(submission.url)
-        file.write('</link>\n</item>\n\n\n')
+        if "youtube" in submission.url:
+            #file.write('<item>\n<title>{}</title>\n<link>{}</link>\n</item>\n  '.format(submission.title, submission.url))
+            file.write('<item>\n<title>')
+            file.write(submission.title)
+            file.write('</title>\n<link>')
+            file.write(submission.url)
+            file.write('</link>\n<thumbnail>')
+            file.write('http://mirrors.kodi.tv/addons/leia/plugin.video.youtube/icon.png')
+            file.write('</thumbnail>\n</item>\n\n\n')
+        elif "twitch" in submission.url:
+            file.write('<plugin>\n<title>')
+            file.write(submission.title)
+            file.write('</title>\n<link>')
+            file.write(submission.url)
+            file.write('</link>\n<thumbnail>')
+            file.write('http://mirrors.kodi.tv/addons/leia/plugin.video.twitch/icon.png')
+            file.write('</thumbnail>\n</plugin>\n\n\n')
+        elif "playlist" in submission.url:
+            file.write('<plugin>\n<title>')
+            file.write(submission.title)
+            file.write('</title>\n<link>')
+            file.write(submission.url)
+            file.write('</link>\n<thumbnail>')
+            file.write('http://mirrors.kodi.tv/addons/leia/plugin.video.youtube/icon.png')
+            file.write('</thumbnail>\n</plugin>\n\n\n')
 
 in_ = open('../Overwatchleague/owl.xml').read()
 out = open('../Overwatchleague/owl.xml', 'w')
-replacements = {'https://www.youtube.com/playlist?list=':'plugin://plugin.video.youtube/playlist/', 'https://m.youtube.com/watch?v=':'https://youtube.com/watch?v=', 'https://m.youtube.com/watch?v=':'https://youtube.com/watch?v=', 'https://clips.twitch.tv/':'plugin://plugin.video.twitch/?use_player=True&mode=play&amp;slug='  ''}
+replacements = {'https://www.youtube.com/playlist?list=':'plugin://plugin.video.youtube/playlist/', 'https://m.youtube.com/watch?v=':'https://youtube.com/watch?v=', 'https://clips.twitch.tv/':'plugin://plugin.video.twitch/?use_player=True&mode=play&amp;slug='  ''}
 for i in replacements.keys():
     in_ = in_.replace(i, replacements[i])
 out.write(in_)
--- utlities/OverwatchUniversity.py
+++ utlities/OverwatchUniversity.py
@@ -13,19 +13,37 @@ if os.path.exists("../Overwatchleague/owu.xml"):
     os.remove('../Overwatchleague/owu.xml')
     
     
-for submission in reddit.subreddit('OverwatchUniversity').search('site:youtube.com', limit=100, sort='hot', time_filter='all'):
+for submission in reddit.subreddit('OverwatchUniversity').search('site:youtube.com OR site:clips.twitch.tv', limit=100, sort='hot', time_filter='all'):
     print(submission.title)
-    with open('../Overwatchleague/owu.xml', 'a') as file:
-        #file.write('<item>\n<title>{}</title>\n<link>{}</link>\n</item>\n  '.format(submission.title, submission.url))
-        file.write('<item>\n<title>')
-        file.write(submission.title)
-        file.write('</title>\n<link>')
-        file.write(submission.url)
-        file.write('</link>\n</item>\n\n\n')
+        if "youtube" in submission.url:
+            #file.write('<item>\n<title>{}</title>\n<link>{}</link>\n</item>\n  '.format(submission.title, submission.url))
+            file.write('<item>\n<title>')
+            file.write(submission.title)
+            file.write('</title>\n<link>')
+            file.write(submission.url)
+            file.write('</link>\n<thumbnail>')
+            file.write('http://mirrors.kodi.tv/addons/leia/plugin.video.youtube/icon.png')
+            file.write('</thumbnail>\n</item>\n\n\n')
+        elif "twitch" in submission.url:
+            file.write('<plugin>\n<title>')
+            file.write(submission.title)
+            file.write('</title>\n<link>')
+            file.write(submission.url)
+            file.write('</link>\n<thumbnail>')
+            file.write('http://mirrors.kodi.tv/addons/leia/plugin.video.twitch/icon.png')
+            file.write('</thumbnail>\n</plugin>\n\n\n')
+        elif "playlist" in submission.url:
+            file.write('<plugin>\n<title>')
+            file.write(submission.title)
+            file.write('</title>\n<link>')
+            file.write(submission.url)
+            file.write('</link>\n<thumbnail>')
+            file.write('http://mirrors.kodi.tv/addons/leia/plugin.video.youtube/icon.png')
+            file.write('</thumbnail>\n</plugin>\n\n\n')
 
 in_ = open('../Overwatchleague/owu.xml').read()
 out = open('../Overwatchleague/owu.xml', 'w')
-replacements = {'https://www.youtube.com/playlist?list=':'plugin://plugin.video.youtube/playlist/', 'https://m.youtube.com/watch?v=':'https://youtube.com/watch?v=', 'https://m.youtube.com/watch?v=':'https://youtube.com/watch?v=', 'https://clips.twitch.tv/':'plugin://plugin.video.twitch/?use_player=True&mode=play&amp;slug='  ''}
+replacements = {'https://www.youtube.com/playlist?list=':'plugin://plugin.video.youtube/playlist/', 'https://m.youtube.com/watch?v=':'https://youtube.com/watch?v=', 'https://clips.twitch.tv/':'plugin://plugin.video.twitch/?use_player=True&mode=play&amp;slug='  ''}
 for i in replacements.keys():
     in_ = in_.replace(i, replacements[i])
 out.write(in_)
